164340d6c6331bca89a15d274f44cdb724fad12f
alphadzingina <alphaadx6@gmail.com>
2020-05-18 12:52:57 +0100

    initialise state object

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({ lat: position.coords.latitude});
+            },
             (err) => console.log(err)
         );
-    
+    }
+    render() {
         return(
-            <div>Latitide: </div>
+            <div>Latitude: {this.state.lat}</div>
         );
     };
 };
