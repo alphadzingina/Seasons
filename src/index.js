b1d678ee64f0421c6ecc10e7b8fc88e02a9b4976
alphadzingina <alphaadx6@gmail.com>
2020-05-18 16:11:45 +0100

    refactor constructor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, errorMessage:'' };
-    }
+    
+    state = { lat: null, errorMessage:'' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
